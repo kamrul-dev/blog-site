2fb918549a71901280da74d5bf8163af14e6d176
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-02 12:38:25 +0600

    active link implemented

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -8,12 +8,12 @@ const Navbar = () => {
             <div>
                 <img className='w-40 h-10' src="https://s3images.coroflot.com/user_files/individual_files/large_677882_ewz3wdcc9ao7eu5f4dlszexu_.png" alt="" />
             </div>
-            <div className='text-lg font-bold'>
-                <NavLink to='/' className='mr-3 p-3'>Home</NavLink>
-                <NavLink to='/videos' className='mr-3 p-3'>Videos</NavLink>
-                <NavLink to='/contact' className='mr-3 p-3'>Contact</NavLink>
-                <NavLink to='/about' className='mr-3 p-3'>About US</NavLink>
-                <NavLink to='/login' className='mr-3 p-3'>Login</NavLink>
+            <div className='text-lg font-bold text-sky-400'>
+                <NavLink to='/' className={({isActive}) => (isActive ? 'text-sky-700 mr-3 p-3' : 'mr-3 p-3')}>Home</NavLink>
+                <NavLink to='/videos' className={({isActive}) => (isActive ? 'text-sky-700 mr-3 p-3' : 'mr-3 p-3')}>Videos</NavLink>
+                <NavLink to='/contact' className={({isActive}) => (isActive ? 'text-sky-700 mr-3 p-3' : 'mr-3 p-3')}>Contact</NavLink>
+                <NavLink to='/about' className={({isActive}) => (isActive ? 'text-sky-700 mr-3 p-3' : 'mr-3 p-3')}>About US</NavLink>
+                <NavLink to='/login' className={({isActive}) => (isActive ? 'text-sky-700 mr-3 p-3' : 'mr-3 p-3')}>Login</NavLink>
             </div>
         </nav>
     );
