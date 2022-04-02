b83e9e84bce9002ab6941a3910591cc81fcb6c37
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-02 10:45:04 +0600

    folder structured

---
+++ src/Components/BlogDetails/BlogDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './BlogDetails.css';
+
+const BlogDetails = () => {
+    return (
+        <div>
+            <h2>This is Blog Details page</h2>
+        </div>
+    );
+};
+
+export default BlogDetails;
