eea3a488faec3b21c6d56fa9f0fa21e7bd22f311
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-02 12:22:48 +0600

    Routing setup done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import Videos from './Components/Videos/Videos';
+import Contact from './Components/Contact/Contact';
+import About from './Components/About/About';
+import Login from './Components/Login/Login';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <h2 className="text-center text-3xl font-bold underline">Welcome to my blog</h2>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/videos' element={<Videos></Videos>}></Route>
+        <Route path='/contact' element={<Contact></Contact>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <div>
-            <h2>This is Navbar</h2>
-        </div>
+        <nav className='flex justify-between items-center shadow-md p-4'>
+            <div>
+                <img className='w-40 h-10' src="https://s3images.coroflot.com/user_files/individual_files/large_677882_ewz3wdcc9ao7eu5f4dlszexu_.png" alt="" />
+            </div>
+            <div className='text-lg font-bold'>
+                <NavLink to='/' className='mr-3 p-3'>Home</NavLink>
+                <NavLink to='/videos' className='mr-3 p-3'>Videos</NavLink>
+                <NavLink to='/contact' className='mr-3 p-3'>Contact</NavLink>
+                <NavLink to='/about' className='mr-3 p-3'>About US</NavLink>
+                <NavLink to='/login' className='mr-3 p-3'>Login</NavLink>
+            </div>
+        </nav>
     );
 };
 
